9c7e64ba744b031c202ead508c36909abfed8b86
João Paulo Pulga <joaopaulo1339@outlook.com>
2019-02-20 11:26:33 -0300

    Added atcix web and license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use actix_web::{http, middleware, server, App, HttpRequest, HttpResponse, Result};
+use env_logger;
+use log;
+
+struct AppState {}
+
+fn index(req: &HttpRequest) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().content_type("text/html").body("Hello"))
+}
+
 fn main() {
-    println!("Hello, world!");
+    ::std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    log::info!("Initialalizng actix web...");
+
+    server::new(move || {
+        App::new()
+            .middleware(middleware::Logger::default())
+            .resource("/", |r| r.method(http::Method::GET).f(index))
+    })
+    .bind("127.0.0.1:8080")
+    .expect("Could not bind to port 8080")
+    .run();
 }
